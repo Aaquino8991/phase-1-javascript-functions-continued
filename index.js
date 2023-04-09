5017a239f9d733ba7d94642a879404849af4adff
Anthony <aquinoanthony851@gmail.com>
2023-04-09 00:14:37 -0600

    JS Functions Contd.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity1 = 'roller-skate', activity2 = 'hike') {
+   return (`This Saturday, I want to ${activity1}!`);
+}
+
+function mondayWork(go = "go to the office") {
+  return (`This Monday, I will ${go}.`)
+}
+
+function wrapAdjective(visualFlair = "*", word) {
+  return function (word = 'special') {
+    return `You are ${visualFlair}${word}${visualFlair}!`
+    
+  }
+}
+
+const encouragingPromptFunction = wrapAdjective("||")("a dedicated programmer");
